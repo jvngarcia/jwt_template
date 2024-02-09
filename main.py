474ba6d394478d7ebef90892ab07c57c4a5c7775
Angel Garcia <angelgarciaweb@gmail.com>
2024-02-09 12:29:28 -0400

    Add database initialization and session management

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from dotenv import load_dotenv
 
 from app.Auth.Login.Infraestructure import Login
 
+from config.db.db import engine, SessionLocal
+from config.db import models
+
+models.Base.metadata.create_all(bind=engine)
+
 load_dotenv()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 app = FastAPI()
